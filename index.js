fd0eaba0ae7897df826192dce6cb2159caf0ea8c
Jooyon Won <jyon1204@gmail.com>
2019-06-26 18:34:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (name, email) => {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object['id'];
+    })
+    .catch(function(error) {
+      alert("Error");
+      document.body.innerHTML = error;
+    });
+}
